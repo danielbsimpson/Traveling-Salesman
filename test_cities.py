0eb16a45c3d88222179d79c79eef7a7d89a17f02
danielbsimpson <40238765+danielbsimpson@users.noreply.github.com>
2020-01-05 18:43:11 +0000

    added more tests

--- test_cities.py
+++ test_cities.py
@@ -1,3 +1,4 @@
+
 import pytest
 from cities import *
 
@@ -8,7 +9,27 @@ def test_compute_total_distance():
 
     road_map2 = [("Alabama", "Montgomery", 32.361538, -86.279118),\
                  ("Alaska",	"Juneau", 58.301935, -134.41974),\
-                 ("Arizona", "Phoenix",	33.448457, -112.073844)]    
+                 ("Arizona", "Phoenix",	33.448457, -112.073844)]
+    
+    country_capital1 = [('Somaliland', 'Hargeisa', 9.55, 44.05),\
+                       ('South Georgia and South Sandwich Islands', \
+                        'King Edward Point', -54.283333, -36.5), \
+                        ('French Southern and Antarctic Lands', \
+                         'Port-aux-FranÃ§ais', -49.35, 70.216667),\
+                        ('Palestine', 'Jerusalem', 31.76666667, 35.233333),\
+                        ('Aland Islands', 'Mariehamn', 60.116667, 19.9)] 
+    
+    country_capital2 = [('Belgium', 'Brussels', 50.83333333, 4.333333),\
+                        ('Belize', 'Belmopan', 17.25, -88.766667),\
+                        ('Benin', 'Porto-Novo', 6.483333333, 2.616667),\
+                        ('Bermuda', 'Hamilton', 32.28333333, -64.783333)]
+    
+    country_capital3 = [('Niger', 'Niamey', 13.51666667, 2.116667),\
+                        ('Nigeria', 'Abuja', 9.083333333, 7.533333),\
+                        ('Rwanda', 'Kigali', -1.95, 30.05),\
+                        ('Saint Helena', 'Jamestown', -15.933333, -5.716667),\
+                        ('Sao Tome and Principe', 'Sao Tome', 0.3333, 6.7333),\
+                        ('Senegal', 'Dakar', 14.73333333, -17.633333)]
 
     assert compute_total_distance(road_map1)==\
            pytest.approx(9.386+18.496+10.646, 0.01)
@@ -25,7 +46,15 @@ def test_swap_cities():
     
     state_city2 = [("Alabama", "Montgomery", 32.361538, -86.279118),\
                  ("Alaska",	"Juneau", 58.301935, -134.41974),\
-                 ("Arizona", "Phoenix",	33.448457, -112.073844)] 
+                 ("Arizona", "Phoenix",	33.448457, -112.073844)]
+    
+    country_capital = [('Somaliland', 'Hargeisa', 9.55, 44.05),\
+                       ('South Georgia and South Sandwich Islands', \
+                        'King Edward Point', -54.283333, -36.5), \
+                        ('French Southern and Antarctic Lands', \
+                         'Port-aux-FranÃ§ais', -49.35, 70.216667),\
+                        ('Palestine', 'Jerusalem', 31.76666667, 35.233333),\
+                        ('Aland Islands', 'Mariehamn', 60.116667, 19.9)]
     
     assert swap_cities(state_city1, 0, 1) == \
          ([("Delaware", "Dover", 39.161921, -75.526755),\
@@ -51,6 +80,24 @@ def test_swap_cities():
           ('Alabama', 'Montgomery', 32.361538, -86.279118)],
             113.92444731706529)    
         
+    assert swap_cities(country_capital, 0, 1) == \
+        ([('South Georgia and South Sandwich Islands', \
+           'King Edward Point', -54.283333, -36.5),
+            ('Somaliland', 'Hargeisa', 9.55, 44.05),\
+            ('French Southern and Antarctic Lands', \
+             'Port-aux-FranÃ§ais', -49.35, 70.216667),\
+             ('Palestine', 'Jerusalem', 31.76666667, 35.233333),\
+             ('Aland Islands', 'Mariehamn', 60.116667, 19.9)])
+        
+    assert swap_cities(country_capital, 0, 4) == \
+        [('Aland Islands', 'Mariehamn', 60.116667, 19.9),\
+                       ('South Georgia and South Sandwich Islands', \
+                        'King Edward Point', -54.283333, -36.5), \
+                        ('French Southern and Antarctic Lands', \
+                         'Port-aux-FranÃ§ais', -49.35, 70.216667),\
+                        ('Palestine', 'Jerusalem', 31.76666667, 35.233333),\
+                        ('Somaliland', 'Hargeisa', 9.55, 44.05)]
+        
     '''add your tests'''
 
 def test_shift_cities():
@@ -62,6 +109,26 @@ def test_shift_cities():
     state_city2 = [("Alabama", "Montgomery", 32.361538, -86.279118),\
                  ("Alaska",	"Juneau", 58.301935, -134.41974),\
                  ("Arizona", "Phoenix",	33.448457, -112.073844)]
+
+    country_capital1 = [('Somaliland', 'Hargeisa', 9.55, 44.05),\
+                       ('South Georgia and South Sandwich Islands', \
+                        'King Edward Point', -54.283333, -36.5), \
+                        ('French Southern and Antarctic Lands', \
+                         'Port-aux-FranÃ§ais', -49.35, 70.216667),\
+                        ('Palestine', 'Jerusalem', 31.76666667, 35.233333),\
+                        ('Aland Islands', 'Mariehamn', 60.116667, 19.9)]
+    
+    country_capital2 = [('Belgium', 'Brussels', 50.83333333, 4.333333),\
+                        ('Belize', 'Belmopan', 17.25, -88.766667),\
+                        ('Benin', 'Porto-Novo', 6.483333333, 2.616667),\
+                        ('Bermuda', 'Hamilton', 32.28333333, -64.783333)]
+    
+    country_capital3 = [('Niger', 'Niamey', 13.51666667, 2.116667),\
+                        ('Nigeria', 'Abuja', 9.083333333, 7.533333),\
+                        ('Rwanda', 'Kigali', -1.95, 30.05),\
+                        ('Saint Helena', 'Jamestown', -15.933333, -5.716667),\
+                        ('Sao Tome and Principe', 'Sao Tome', 0.3333, 6.7333),\
+                        ('Senegal', 'Dakar', 14.73333333, -17.633333)]
     
     assert shift_cities(state_city1) == \
         ([("Minnesota", "Saint Paul", 44.95, -93.094),\
@@ -74,6 +141,29 @@ def test_shift_cities():
          ("Alabama", "Montgomery", 32.361538, -86.279118),\
          ("Alaska",	"Juneau", 58.301935, -134.41974)],\
         113.92444731706527)
+    
+    assert shift_cities(country_capital1) == \
+        ([('Aland Islands', 'Mariehamn', 60.116667, 19.9),
+          ('Somaliland', 'Hargeisa', 9.55, 44.05),\
+          ('South Georgia and South Sandwich Islands', \
+                        'King Edward Point', -54.283333, -36.5), \
+          ('French Southern and Antarctic Lands', \
+                         'Port-aux-FranÃ§ais', -49.35, 70.216667),\
+           ('Palestine', 'Jerusalem', 31.76666667, 35.233333)])
+          
+    assert shift_cities(country_capital2) == \
+        ([('Bermuda', 'Hamilton', 32.28333333, -64.783333),\
+          ('Belgium', 'Brussels', 50.83333333, 4.333333),\
+          ('Belize', 'Belmopan', 17.25, -88.766667),\
+          ('Benin', 'Porto-Novo', 6.483333333, 2.616667)])
+    
+    assert shift_cities(country_capital3) == \
+        ([('Senegal', 'Dakar', 14.73333333, -17.633333),
+          ('Niger', 'Niamey', 13.51666667, 2.116667),\
+          ('Nigeria', 'Abuja', 9.083333333, 7.533333),\
+          ('Rwanda', 'Kigali', -1.95, 30.05),\
+          ('Saint Helena', 'Jamestown', -15.933333, -5.716667),\
+          ('Sao Tome and Principe', 'Sao Tome', 0.3333, 6.7333)])
 
          
     '''add your tests'''
