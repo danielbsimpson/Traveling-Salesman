e870c8d8cdc3532c9193337cbdeb8ac200a2002d
danielbsimpson <40238765+danielbsimpson@users.noreply.github.com>
2020-01-05 18:42:38 +0000

    finished scaling visualize function

--- cities.py
+++ cities.py
@@ -138,38 +138,41 @@ def print_map(best_cycle):
     """
     pass
 
-def visualize_road_map(best_cycle):
+def visualize(best_cycle):
     
     long_list = []
     lat_list = []
+    #pull the longitude and latitude from best_cycle
     for j in range(0, len(best_cycle)):
         long_list.append(float(best_cycle[j][3]))
         lat_list.append(float(best_cycle[j][2]))
-    
+    #sort the data
     long_list.sort()
     lat_list.sort()
-    
+    #get the smallest and largest longitude/latitude and round up/down
     long_min = math.floor(long_list[0])
-    long_max = math.ceil(long_list[49])
+    long_max = math.ceil(long_list[len(long_list) - 1])
     lat_min = math.floor(lat_list[0])
-    lat_max = math.ceil(lat_list[49])
+    lat_max = math.ceil(lat_list[len(lat_list) - 1])
     
     #set min and max long/lat to max values in list or -180/180 and -90/90
     x1_coordinate = max(long_min, -180)
     x2_coordinate = min(long_max, 180)    
     y1_coordinate = min(lat_min, 90)
     y2_coordinate = max(lat_max, -90)
-    
+    #total height/width in latitude/longitude
     distance_x = (x2_coordinate - x1_coordinate)
     distance_y = (y2_coordinate - y1_coordinate)
-    
+    #create a window the size of the monitor
     window = tk.Tk()
     window.title("Road Map")
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
-
-    canvas = tk.Canvas(window, bg = 'white', width = screen_width - 100, height = screen_height - 100)
+    #create a blank canvas the size of the window
+    canvas = tk.Canvas(window, bg = 'white', width = screen_width, height = screen_height)
     canvas.pack()
+    #identify how large the grid will be on the blank canvas
+    #space left on right side for labels created later on
     grid_size_w_start = 30
     grid_size_h_start = 30
     grid_size_w = screen_width - 400
@@ -178,6 +181,7 @@ def visualize_road_map(best_cycle):
     total_grid_w = grid_size_w - grid_size_w_start
     total_grid_h = grid_size_h - grid_size_h_start
     
+    #pixel and latitude/longitude conversions with rounding
     x_line_distance = int(total_grid_w / distance_x)
     y_line_distance = int(total_grid_h / distance_y)
     
@@ -186,30 +190,52 @@ def visualize_road_map(best_cycle):
     
     x_axis_values = math.ceil(distance_x / x_line_total)
     y_axis_values = math.ceil(distance_y / y_line_total)
-    
+    #scale values for when there are 80 or less cities vs 80 or more cities to plot
+    if len(best_cycle) < 80:
+        font_city_size = 8
+        space_size = 10
+        axis_label_size = 6
+        label_count_value = 1
+        grid_city_labels = 10
+        xlines_scale = 40
+        ylines_scale = 35
+        limiting_value = 0
+        wording_center1 = 200
+    else:
+        font_city_size = 5
+        space_size = 8
+        axis_label_size = 5
+        label_count_value = 5
+        grid_city_labels = 8
+        xlines_scale = 0
+        ylines_scale = 0
+        limiting_value = 35
+        wording_center1 = 100
+        wording_center2 = 300
+        
     #draw vertical lines:    
-    y_2 = grid_size_h_start + ((y_line_total) * y_line_distance)
-    for i in range(grid_size_h_start, ((x_line_total)* x_line_distance) + 40, x_line_distance):
+    y_2 = grid_size_h_start + ((y_line_total) * y_line_distance) - limiting_value
+    for i in range(grid_size_h_start, ((x_line_total)* x_line_distance) + ylines_scale, x_line_distance):
         canvas.create_line(i,grid_size_h_start,i,y_2)
     
     #draw horizontal lines:        
-    x_2 = grid_size_w_start + ((x_line_total) * x_line_distance)
-    for i in range(grid_size_w_start, ((y_line_total)* y_line_distance) + 35, y_line_distance):
+    x_2 = grid_size_w_start + ((x_line_total) * x_line_distance) - limiting_value
+    for i in range(grid_size_w_start, ((y_line_total)* y_line_distance) + xlines_scale, y_line_distance):
         canvas.create_line(grid_size_w_start,i,x_2,i)
         
     #label x-axis:
     count_x = x1_coordinate
-    for i in range(0, (x_line_total + 1) * x_line_distance, x_line_distance):
+    for i in range(0, (x_line_total + 1) * x_line_distance - limiting_value, x_line_distance*label_count_value):
         x = grid_size_w_start + i
         y = 20
-        canvas.create_text(x, y, text = count_x, fill = 'blue', font = ("Times", 6, 'bold'))
-        count_x += x_axis_values
+        canvas.create_text(x, y, text = count_x, fill = 'blue', font = ("Times", axis_label_size, 'bold'))
+        count_x += x_axis_values*label_count_value
     #label y-axis:
     count_y = y2_coordinate
-    for i in range(0,(y_line_total + 1)*y_line_distance, y_line_distance):
+    for i in range(0,(y_line_total + 1)*y_line_distance - limiting_value, y_line_distance):
         x = 10
         y = grid_size_h_start + i
-        canvas.create_text(x,y, text = count_y, fill = 'blue', font = ("Times", 6, 'bold'))
+        canvas.create_text(x,y, text = count_y, fill = 'blue', font = ("Times", axis_label_size, 'bold'))
         count_y -= y_axis_values
      
     
@@ -222,17 +248,24 @@ def visualize_road_map(best_cycle):
         x_position = grid_size_w_start - int(((x1_coordinate - x) * (x_line_distance / x_axis_values)))
         y_position = grid_size_h_start + int(((y2_coordinate - y) * (y_line_distance / y_axis_values)))
         
-        canvas.create_text(x_position, y_position, text = str(count_city), fill = 'red', font = ("Times", 10, "bold"))
+        canvas.create_text(x_position, y_position, text = str(count_city), fill = 'red', font = ("Times", grid_city_labels, "bold"))
         count_city += 1
     
-    #label numbers with city, state
     y_text = 30
+    y_count = 30
+
+    #label numbers with city, state
     for i in range(0, (len(best_cycle))):
         city = str(best_cycle[i][1])
         state = str(best_cycle[i][0])
         full_text = str(i+1) + '.' + city + ',' + state
-        canvas.create_text(grid_size_w + 100, y_text, text = full_text, font = ("Times", 8, 'bold') )
-        y_text += 10
+        #if less than 125 cities, we stick with one column, over 125 we start a new column
+        if i < 125:
+            canvas.create_text(grid_size_w + wording_center1, y_text, text = full_text, font = ("Times", font_city_size, 'bold') )
+        else:
+            canvas.create_text(grid_size_w + wording_center2, y_count, text = full_text, font = ("Times", font_city_size, 'bold') )
+            y_count += space_size            
+        y_text += space_size
         
         
     
@@ -243,7 +276,7 @@ def visualize_road_map(best_cycle):
 
 
 def main():
-    road_map = read_cities('city-data.txt')
+    road_map = read_cities('asia.txt')
     print()
     print('Original Data:' + '\n')
     print(road_map)
@@ -252,7 +285,7 @@ def main():
     print()
     print('Best cycle road map with distance between cities:' + '\n')
     print(printed_map)
-    visualize_road_map(best_cycle[0])
+    visualize(best_cycle[0])
     
 
     """
